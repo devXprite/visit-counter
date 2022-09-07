53ca95eeddfb3a3f60e1b745b812111a166a0055
DevXprite <devxprite@gmail.com>
2022-09-07 13:38:34 +0530

    update

--- routes/counter.js
+++ routes/counter.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const createImage = require("../util/createImage");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    const input = String(Math.floor(Math.random() * 5000000000));
+    const {
+        s: fontSize = 40,
+        url: ID = "default",
+        c: textColor = "00ff00",
+        ff: fontFamily = "dsdigi",
+        bg: backgroundColor = "00000000",
+    } = req.query;
 
-    const image = createImage(input);
+    const counter = String(Math.floor(Math.random() * 1000000000));
+    // const counter = String(8);
+    const image = createImage(counter, fontSize, fontFamily, textColor, backgroundColor);
     res.writeHead(200, { "Content-Type": "image/png" });
 
     res.end(image);
--- util/createImage.js
+++ util/createImage.js
@@ -1,34 +1,40 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable unicorn/prefer-default-parameters */
 const { createCanvas, GlobalFonts } = require("@napi-rs/canvas");
 
-// const {
-//   writeFileSync,
-// } = require('fs');
+const calculateTextWidth = (text, fontSetting) => {
+    const ctx = createCanvas(1, 1).getContext("2d");
+    ctx.font = fontSetting;
+    const { width } = ctx.measureText(text);
+    return width;
+};
 
-const createImage = (input, fontSize, fontName, color) => {
-    const CanvasFontSize = fontSize || 40;
-    const fontFamily = fontName || "DS_DIGI";
-    const textColor = color || "#00ff00";
-    const fontSetting = `${CanvasFontSize}px '${fontFamily}'`;
+const createImage = (input, fontSize, fontFamily, textColor, backgroundColor) => {
+    GlobalFonts.registerFromPath(`${__dirname}/font/DS-DIGI.ttf`, "dsdigi");
+    GlobalFonts.registerFromPath(`${__dirname}/font/DS-DIGII.ttf`, "dsdigii");
+    GlobalFonts.registerFromPath(`${__dirname}/font/Electrolize.ttf`, "electrolize");
 
-    const height = CanvasFontSize * 1.2;
-    const width = input.length * (CanvasFontSize / 1.8);
+    const fontSetting = `${fontSize}px '${fontFamily}'`;
+    const textWidth = calculateTextWidth(input, fontSetting);
 
-    GlobalFonts.registerFromPath(`${__dirname}/font/DS-DIGI.TTF`, "DS_DIGI");
+    const height = fontSize * 1.15;
+    const width = textWidth + (textWidth * (4 / 100)) + 12;
 
     const canvas = createCanvas(width, height);
     const ctx = canvas.getContext("2d");
 
-    ctx.font = fontSetting;
-    ctx.fillStyle = textColor;
+    ctx.fillStyle = /(\d)/g.test(backgroundColor) ? `#${backgroundColor}` : backgroundColor;
+    ctx.fillRect(0, 0, width, height);
 
-    ctx.fillText(input, CanvasFontSize / 20, canvas.height / 1.3);
+    ctx.font = fontSetting;
+    ctx.fillStyle = /(\d)/g.test(textColor) ? `#${textColor}` : textColor;
+
+    ctx.fillText(input, (width - textWidth) / 2, height / 1.3);
     ctx.textAlign = "center";
 
     const buffer = canvas.toBuffer("image/png");
-    //   writeFileSync(join(__dirname, 'demo.png'), buffer);
 
     return buffer;
 };
 
-// createImage('00008', 24, null, '#0ff');
 module.exports = createImage;
